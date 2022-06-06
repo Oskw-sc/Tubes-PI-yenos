96397ea69cda1833a5bcbdad241c2c16cf61782b
Hendry Winata <hendry24680@gmail.com>
2022-06-06 16:24:22 +0700

    perbaikan response create category + construct

--- yenos-news/app/Controllers/Category.php
+++ yenos-news/app/Controllers/Category.php
@@ -17,6 +17,15 @@ class Category extends ResourceController
         $this->categorymodel = new CategoryModel();
     }
 
+    private function auth_token($auth_token_header)
+    {
+        if ($auth_token_header) {
+            $key = getenv('JWT_SECRET');
+            $auth_token_value = $auth_token_header->getValue();
+            return JWT::decode($auth_token_value, new Key($key, 'HS256'));
+        } else return null;
+    }
+
     // public function auth()
     // {
     //     $key = getenv('JWT_SECRET');
@@ -83,66 +92,77 @@ class Category extends ResourceController
 
     public function create()
     {
-        $key = getenv('JWT_SECRET');
-        $authHeader = $this->request->getHeader("Authorization");
-        if(!$authHeader) return $this->failUnauthorized('auth-token must be passed as header request');
-        $token = $authHeader->getValue();
-        try {
-            $decoded = JWT::decode($token, new Key($key, 'HS256'));
-            $level = $decoded->data->acc_level;
-            if($level != "admin") return $this->failForbidden('You dont have a permission to add category');
-
-            if ($decoded && ($decoded->exp - time() > 0)) {
-
-                $rules = [
-                    "name" => "required|max_length[255]",
-                ];
-
-                $messages = [
-                    "name" => [
-                        "required" => "category is required"
-                    ],
-                ];
-
-                if (!$this->validate($rules, $messages)) {
+        $token_decoded = $this->auth_token($this->request->getHeader('auth-token'));
+        if (!$token_decoded) {
+            $response = [
+                'status' => 401,
+                'error' => true,
+                'message' => 'auth-token must be set as header request',
+            ];
+        } else {
+            try {
+                $level = $token_decoded->data->acc_level;
+                if($level != "admin") {
                     $response = [
-                        'status' => 500,
+                        'status' => 403,
                         'error' => true,
-                        'message' => $this->validator->getErrors(),
-                        'data' => []
+                        'message' => 'Current account does not have permission to create category',
                     ];
                 } else {
-
-                    $data = [
-                        "name" => $this->request->getVar("name"),
-                    ];
-
-                    if ($this->categorymodel->insert($data)) {
-                        $response = [
-                            'status' => 201,
-                            "error" => false,
-                            'messages' => 'Category has been added successfully',
-                            'data' => []
+                    if ($token_decoded && ($token_decoded->exp - time() > 0)) {
+                        $rules = [
+                            "name" => "required|max_length[255]",
                         ];
+                        $messages = [
+                            "name" => [
+                                "required" => "category'name is required",
+                                "max_length" => "category's name can be filled by a maximum of 255 characters",
+                            ],
+                        ];
+
+                        if (!$this->validate($rules, $messages)) {
+                            $response = [
+                                'status' => 400,
+                                'error' => true,
+                                'message' => $this->validator->getErrors(),
+                            ];
+                        } else {
+                            $data = [
+                                "name" => $this->request->getVar("name"),
+                            ];
+                
+                            if ($this->categorymodel->insert($data)) {
+                                $response = [
+                                    'status' => 201,
+                                    "error" => false,
+                                    'messages' => 'Category has been added successfully',
+                                ];
+                            } else {
+                                $response = [
+                                    'status' => 500,
+                                    "error" => true,
+                                    'messages' => 'Internal server error, please try again later',
+                                ];
+                            }
+                        }
                     } else {
                         $response = [
-                            'status' => 500,
-                            "error" => true,
-                            'messages' => 'Failed to create Category',
-                            'data' => []
+                            'status' => 401,
+                            'error' => true,
+                            'message' => 'auth-token is invalid, might be expired',
                         ];
                     }
                 }
-
-                return $this->respondCreated($response);
+            } catch (Exception $ex) {
+                $response = [
+                    'status' => 401,
+                    'error' => true,
+                    'message' => 'auth-token is invalid, might be expired',
+                ];
             }
-        } catch (Exception $ex) {
-            $response = [
-                'status' => 401,
-                'messages' => 'auth-token is invalid, might be expired',
-            ];
-            return $this->respondCreated($response);
         }
+
+        return $this->respond($response, $response['status']);
     }
 
     public function update($id = null)
